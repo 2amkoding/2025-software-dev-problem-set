21bc1e51a678188e07f93d90693cd131c46931b2
2amkoding <2amkoding@gmail.com>
2025-01-26 11:07:24 -0600

    refactor for optimization: linear time complexity and consant space usage

--- solutions/2-Valid Anagram.js
+++ solutions/2-Valid Anagram.js
@@ -1,9 +1,18 @@
 var isAnagram = function(s, t) {
-let s1 = s.split('').sort().join('')
-let t2 = t.split('').sort().join('')
 
-if (s1 == t2){
-    return true
+if(s.length !== t.lengthj) {
+return false
 }
- return false
+
+const count = new Array(26).fill(0);
+
+for(let i =0; i<s.length; i++) {
+    count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++
+    count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--
+}
+return count.every(val => val === 0);
 };
+
+//linear time complexity and constant space usage = optimization for large inputs
+//Time: O(n), Space: O(1)
+//sort & split = Time: O(n logn), Space: O(n)
