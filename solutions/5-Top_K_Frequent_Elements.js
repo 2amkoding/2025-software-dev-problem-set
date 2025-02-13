d43f04e6bc1c2eb3a7a0e39bd781ee60342203ac
2amkoding <2amkoding@gmail.com>
2025-02-13 12:51:57 -0600

    created Buckets + Frequency Counter

--- solutions/5-Top_K_Frequent_Elements.js
+++ solutions/5-Top_K_Frequent_Elements.js
@@ -1,28 +1,14 @@
-//this will require more than 1 day. Possibly 3 days, 1 hr/day.
-
-/*
-Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
-
-Example 1:
-Input: nums = [1,1,1,2,2,3], k = 2
-Output: [1,2]
-
-Example 2:
-Input: nums = [1], k = 1
-Output: [1]
-
-Constraints:
-    1 <= nums.length <= 105
-    -104 <= nums[i] <= 104
-    k is in the range [1, the number of unique elements in the array].
-    It is guaranteed that the answer is unique.
-
-Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
-*/
-
-//THE PLAN:
-
 //Wtf is:
 //frequency map
 //bucket sort approach
 //alternate approach: Heap (priority que)
+
+//freqmap
+let  freq= {}
+
+//create buckets
+const bucketOfFreqs = new Array(nums.length + 1).fill().map(() => []);
+
+for(const n of nums) {
+count[n] = (count[n]|| 0) +1; // "Shorthand"
+}
