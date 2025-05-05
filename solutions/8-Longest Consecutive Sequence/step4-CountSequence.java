d797daec94c9a7707c0790cebfba30091d223f9a
2amkoding <2amkoding@gmail.com>
2025-05-05 16:18:27 -0500

    solution

--- solutions/8-Longest Consecutive Sequence/step4-CountSequence.java
+++ solutions/8-Longest Consecutive Sequence/step4-CountSequence.java
@@ -10,15 +10,27 @@
 // update HighestRecordOfSequence
 // Compare HighestRecordOfSequence vs currentSequence
 
+
 class Solution {
     public int longestConsecutive(int[] nums) {
+
         Set<Integer> numSet = new HashSet<>();
         for (int num : nums) {
             numSet.add(num);
         }
 
         int longest = 0;
+        for(int num : numSet) {
+            if (!numSet.contains(num-1)) {
+                int length = 1;
 
+                while (numSet.contains(num+length)) {
+                    length++;
+                }
+                longest = Math.max(longest,length);
+            }
+        }
 
+        return longest;
     }
 }
