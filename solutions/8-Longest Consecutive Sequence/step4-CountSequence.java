d5ada155db8e453cf81864a453ad9a0d94be0a6a
2amkoding <2amkoding@gmail.com>
2025-05-05 16:08:36 -0500

    Find Starting Value

---
+++ solutions/8-Longest Consecutive Sequence/step4-CountSequence.java
@@ -0,0 +1,21 @@
+//Determine the starting value before counting sequence.
+
+//Starting value
+    // given Set[val1, valu2, etc]
+    //  Loop thru set, if currentValue - 1 exists, currentValue cannot be the startingValue.
+    // currentSequence stays at 1. We are not interested in calculating sequence until finding the startingValue
+
+
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longest = 0;
+
+
+    }
+}
