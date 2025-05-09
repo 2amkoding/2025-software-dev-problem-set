4b7791ac2f7842da97251ac3d31d2a1b57a6f000
2amkoding <2amkoding@gmail.com>
2025-05-09 11:36:20 -0500

    initial thougts

---
+++ solutions/9-ValidPalindrome/2-initialThoughts.java
@@ -0,0 +1,25 @@
+//thoughts
+
+    // " " -> true
+    // "ci vic" -> true
+
+    //loop thru chars? [can u loop thru chars in java?]
+    //if !" ", add char to set
+        {
+            c:1
+            i: 2
+            v: 3
+            i: 4
+            c: 5
+
+                }
+if middle is odd start there
+if middle is even swap 2 values
+leftValue is 1
+rightValue is 5
+if leftValue !== rightValue -> false
+else
+    leftValue++
+    rightValue--
+//or
+
