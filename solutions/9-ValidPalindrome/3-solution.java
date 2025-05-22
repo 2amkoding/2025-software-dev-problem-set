9072baa4db417b66be11059e1e1c1b93ca8d245c
2amkoding <2amkoding@gmail.com>
2025-05-22 14:47:37 -0500

    solution

--- solutions/9-ValidPalindrome/3-solution.java
+++ solutions/9-ValidPalindrome/3-solution.java
@@ -5,10 +5,28 @@
 
 // remove spaces
 // check if alphanumerical.
+//      reggex or isLetterOrDigit()
+
+// convert to lowercase first
+// remove all non-alphanumeric
+// two pointers pattern
+
 
 class Solution {
     public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
+        int left = 0;
+        int right = s.length() - 1;
 
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
+}
 }
