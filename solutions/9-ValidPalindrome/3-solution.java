fe3a6f97aff7250b5ac3a56d1096417312dfd132
2amkoding <2amkoding@gmail.com>
2025-05-22 13:51:02 -0500

    thoughts

---
+++ solutions/9-ValidPalindrome/3-solution.java
@@ -0,0 +1,14 @@
+// https://docs.oracle.com/javase/8/docs/api/java/lang/Character.html
+// toChars()
+// toLowerCase()
+// two pointers pattern
+
+// remove spaces
+// check if alphanumerical.
+
+class Solution {
+    public boolean isPalindrome(String s) {
+
+
+    }
+}
